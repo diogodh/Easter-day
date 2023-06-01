ec4fd81d0b31ef5728ea667a15e6ab1c36159f5d
Diogo Ribeiro <18470550+diogodh@users.noreply.github.com>
2023-06-01 23:49:12 +0100

    Update easter.py
    Cleaning my GitHub

--- easter.py
+++ easter.py
@@ -1,41 +1,59 @@
-#lua 29.53 dias ; 
-#10 jan as 19.21h (235.35h)
+''' The Council of Nicaea in 325 AD defined Easter day as "Easter falls on the first Sunday following the first full moon after the vernal equinox (around March 21st). The vernal equinox is the moment when the sun crosses the celestial equator and day and night are of equal length.
+In this script we will try to find the right day and month of Easter after 2020 AD'''
 
-#lua = 235.35 (29.53 dias)
-#lua_x = 235.35 + 708.72 * x
-#1 ano = 8765.8128
-#10 anos = 8765.8128
+''' 
+Moon orbit 29.53 days 
+10th Jan at 19:21h (235.35h)
+moon = 235.35 (29.53 dias)
+moon_x = 235.35 + 708.72 * x
+sunday = 96 + 168 * x
+'''
 
-#domingo_x = 96 + 168 * x
+# number of hours in a real (astrononimic) year (time earth takes to do a full orbit) 
+year = 8765.8128
 
-x = y = domingo = lua = 0
+#hours from beguining of the year to the equinox
 equinox = 1899.83
-ano = 8765.8128
 
-ano_pascoa = int(input("Ano: "))
-dif_ano = ano_pascoa - 2020
-horas_ano = ano + ano * dif_ano
-equinox_ano = equinox + ano * dif_ano
+#input in that year you want to know the Easter day?
+easter_year = int(input("Ano: "))
 
-while lua < equinox_ano:
-	lua = 235.35 + 708.72 * x
-	x = x + 1
-	
-while domingo < lua:
-	domingo = 96 + (168 * y)
-	y = y + 1
+#Now let's go step by step. I believe its self explainable
+dif_year = easter_year - 2020
+hours_year = year + year * dif_year
+equinox_year = equinox + year * dif_year
 
-dia = domingo / 24
-dia_ano = dia - 365 * dif_ano
+x= 0
 
-if dia_ano > 120.25:
-    mes = "Maio"
-    dia_ano2 = dia_ano - 120.25
-elif dia_ano > 90.25:
-    mes = "Abril"
-    dia_ano2 = dia_ano - 90.25
-elif dia_ano > 59.25:
-    mes = "Marco"
-    dia_ano2 = dia_ano
+# Calculate the lunar cycle until it reaches or exceeds the equinox of the given year
+while True:
+    moon = 235.35 + 708.72 * x
+    if moon >= equinox_year:
+        break
+    x += 1
 
-print("Dia", int(round(dia_ano2)), "de", mes, "de", ano_pascoa)
+y = 0
+# Calculate the Sunday cycle until it reaches or exceeds the lunar cycle
+while True:
+    sundayo = 96 + (168 * y)
+    if sunday >= moon:
+        break
+    y += 1
+
+day = sunday / 24
+day_year = day - 365 * dif_year
+
+# Determine the month and adjusted day of the year based on the calculated values
+if day_year > 120.25:
+    month = "May"
+    day_year2 = day_year - 120.25
+elif day_year > 90.25:
+    month = "April"
+    day_year2 = day_year - 90.25
+else:
+    month = "March"
+    day_year2 = day_year - 59.25
+
+# Print the predicted date of Easter in that year
+
+print("Day ", int(round(day_year2)), "of ", month, "of ", year_easter)
